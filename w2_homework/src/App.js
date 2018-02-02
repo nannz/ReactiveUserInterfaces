53c209802a13d4b5ed8072491f5ec85a4c5a1a3f
Nan Zhao <nanzhao@nyu.edu>
2018-02-02 15:34:50 +0800

    Add <a/>for hyperlink; add hover effect; fix img style

--- w2_homework/src/App.js
+++ w2_homework/src/App.js
@@ -4,6 +4,7 @@ import Article from './Article.js';
 import Content from './Content'
 import Date from './Date'
 import StoryLinkArea from './StoryLinkArea'
+import Thumb from './Thumb'
 
 class App extends Component {
   render() {
@@ -11,35 +12,41 @@ class App extends Component {
       <div className="App">
           <Article articleId = "1">
               <Date date="Jan. 31, 2018"/>
+              <a href = "https://www.nytimes.com/2018/02/01/technology/amazon-apple-holiday-sales.html?rref=collection%2Fsectioncollection%2Fbusiness" target="_blank">
               <StoryLinkArea>
-                <Content
-                  title = "Taiwan Citizens Accused of Violating U.N. Sanctions on North Korea"
-                  summary = "The case comes after another Taiwanese citizen has faced accusations of conducting trade in violation of United Nations sanctions against North Korea."
-                  author = "CHRIS HORTON"/>
-                <img src="https://static01.nyt.com/images/2018/02/01/world/01taiwan-1/01taiwan-1-mediumThreeByTwo210.jpg" className="thumb" />
+                  <Content
+                      title = "Taiwan Citizens Accused of Violating U.N. Sanctions on North Korea"
+                      summary = "The case comes after another Taiwanese citizen has faced accusations of conducting trade in violation of United Nations sanctions against North Korea."
+                      author = "CHRIS HORTON"/>
+                  <Thumb imgSource="https://static01.nyt.com/images/2018/02/01/world/01taiwan-1/01taiwan-1-mediumThreeByTwo210.jpg"/>
               </StoryLinkArea>
+              </a>
           </Article>
 
           <Article articleId = "2">
               <Date date="Jan. 30, 2018"/>
+              <a href = "https://www.nytimes.com/2018/02/01/us/penn-wynn-cosby-metoo.html?rref=collection%2Fsectioncollection%2Fbusiness" target="_blank">
               <StoryLinkArea>
-              <Content
-                  title = "Complaint Accuses Contractor of Underpayment at Medicare Call Centers"
-                  summary = "The Communications Workers of America said in a complaint filed on Friday that a large government information technology contractor could owe workers as much as $100 million in back wages."
-                  author = "JIM TANKERSLEY"/>
-              <img src="https://static01.nyt.com/images/2018/01/31/us/politics/31dc-wagetheft/merlin_133114271_ec906296-f6b1-42bb-b5c9-c8a8a236aa28-mediumThreeByTwo210.jpg" className="thumb" />
+                  <Content
+                      title = "Complaint Accuses Contractor of Underpayment at Medicare Call Centers"
+                      summary = "The Communications Workers of America said in a complaint filed on Friday that a large government information technology contractor could owe workers as much as $100 million in back wages."
+                      author = "JIM TANKERSLEY"/>
+                  <Thumb imgSource="https://static01.nyt.com/images/2018/01/31/us/politics/31dc-wagetheft/merlin_133114271_ec906296-f6b1-42bb-b5c9-c8a8a236aa28-mediumThreeByTwo210.jpg"/>
               </StoryLinkArea>
+              </a>
           </Article>
 
           <Article articleId = "3">
               <Date date="Jan. 29, 2018"/>
+              <a href = "https://www.nytimes.com/2018/02/01/technology/alphabet-fourth-quarter-earnings.html?rref=collection%2Fsectioncollection%2Fbusiness" target="_blank">
               <StoryLinkArea>
-              <Content
-                  title = "Volkswagen Suspends Top Lobbyist Amid Inquiry Into Diesel Tests on Monkeys"
-                  summary = "Thomas Steg, a former top aide to Chancellor Angela Merkel of Germany, exemplified what critics say are the cozy ties between her government and the country’s carmakers."
-                  author = "JACK EWING"/>
-              <img src="https://static01.nyt.com/images/2018/01/31/business/31EUROPEDIESEL/merlin_133099070_acb0dd40-e277-43a9-ac6a-7c9aa447fa51-mediumThreeByTwo210.jpg" className="thumb" />
+                  <Content
+                      title = "Volkswagen Suspends Top Lobbyist Amid Inquiry Into Diesel Tests on Monkeys"
+                      summary = "Thomas Steg, a former top aide to Chancellor Angela Merkel of Germany, exemplified what critics say are the cozy ties between her government and the country’s carmakers."
+                      author = "JACK EWING"/>
+                  <Thumb imgSource="https://static01.nyt.com/images/2018/01/31/business/31EUROPEDIESEL/merlin_133099070_acb0dd40-e277-43a9-ac6a-7c9aa447fa51-mediumThreeByTwo210.jpg"/>
               </StoryLinkArea>
+              </a>
           </Article>
 
       </div>
