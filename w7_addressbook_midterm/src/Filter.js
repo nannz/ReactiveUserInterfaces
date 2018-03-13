b286850364aaca3dc5c6ec9f1f50a2f2d08b605d
Nan Zhao <nanzhao@nyu.edu>
2018-03-13 23:30:02 +0800

    Implement css

--- w7_addressbook_midterm/src/Filter.js
+++ w7_addressbook_midterm/src/Filter.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 import './Filter.css';
 import Checkbox from 'material-ui/Checkbox';
-import ActionFavorite from 'material-ui/svg-icons/action/favorite';
-import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
-import Visibility from 'material-ui/svg-icons/action/visibility';
-import VisibilityOff from 'material-ui/svg-icons/action/visibility-off';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import Divider from 'material-ui/Divider';
 const styles = {
     block: {
-        maxWidth: 250,
+        // maxWidth: 250,
     },
     checkbox: {
-        marginBottom: 16,
+        paddingBottom:16,
+        //marginBottom: 16,
+    },
+    radioButton: {
+        paddingBottom:16,
+        //marginBottom: 16,
     },
 };
 
@@ -23,6 +26,7 @@ class Filter extends Component {
         this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
         this.handleSubmitBtn = this.handleSubmitBtn.bind(this);
         this.handleReset = this.handleReset.bind(this);
+        this.handleExpansionPanel = this.handleExpansionPanel.bind(this);
     }
 
     handleReset() {
@@ -45,6 +49,10 @@ class Filter extends Component {
         this.props.onCloseModal();
     }
 
+    handleExpansionPanel(){
+
+    }
+
     handleSortChange(e) {
         this.props.onChangeSortType(e.target.value); //firstName/lastName/recent
     }
@@ -60,6 +68,7 @@ class Filter extends Component {
         }
         this.props.onChangeCheckbox(updatedCountryCheckboxes);
     }
+
 // <label className="checkbox-custom-label">
 // {c}
 // <input classID={"checkBoxCountry" + this.props.uniqueCountries.indexOf(c)}
@@ -70,18 +79,18 @@ class Filter extends Component {
 // onChange={this.handleCheckboxChange}
 // />
 // </label>
+
     render() {
         const checkboxCountries = this.props.uniqueCountries.map((c, i) => {
             //console.log("this c"+ c);
             return (
-                <Checkbox  key = {i} classID={"checkBoxCountry" + this.props.uniqueCountries.indexOf(c)}
-                           label={c}  labelPosition="left"
-                           value={c} checked={this.props.filterCountries.has(c)}
-                           onCheck={this.handleCheckboxChange}
-                           style={styles.checkbox}/>
+                <Checkbox key={i} classID={"checkBoxCountry" + this.props.uniqueCountries.indexOf(c)}
+                          label={c} labelPosition="left"
+                          value={c} checked={this.props.filterCountries.has(c)}
+                          onCheck={this.handleCheckboxChange}
+                          style={styles.checkbox}/>
             )
         });
-
 
         return (
             <div className="Filter" style={{display: this.props.showFilter ? 'block' : 'none'}}>
@@ -94,47 +103,76 @@ class Filter extends Component {
                 </div>
                 <form onReset={this.handleReset} className="filter-content">
                     <div className="expansionBar">
-                        <h2>Sort By :</h2>
-                        <div className="bar-right">
+                        <div className="bar-left">
+                            <h2>Sort By :</h2>
                             <p>{this.props.currentSortType}</p>
+                        </div>
+                        <button type="button" name="expandBtn" value="expand" onClick={this.handleExpansionPanel}>
+                            <i className="material-icons">expand_more</i>
+                        </button>
+                    </div>
+                    <RadioButtonGroup className="sortTypes" name="sortType" labelPosition="left"
+                                      defaultSelected={this.props.currentSortType}
+                                      onChange={this.handleSortChange}
+                                      style={styles.block}>
+                        <RadioButton
+                            value="First Name"
+                            label="First Name"
+                            style={styles.radioButton}
+                        />
+                        <RadioButton
+                            value="Last Name"
+                            label="Last Name"
+                            style={styles.radioButton}
+                        />
+                        <RadioButton
+                            value="Recently Contacted"
+                            label="Recently Contacted"
+                            style={styles.radioButton}
+                        />
+                    </RadioButtonGroup>
+                    <Divider />
+                    <div className="sortBy">{/*old code*/}
+                        {/*<div>*/}
+                        {/*<label>*/}
+                        {/*<input type="radio" classID="sortFirstName"*/}
+                        {/*name="sortType" value="First Name"*/}
+                        {/*onChange={this.handleSortChange}/>*/}
+                        {/*First Name*/}
+                        {/*</label>*/}
+                        {/*</div>*/}
+                        {/*<div>*/}
+                        {/*<label>*/}
+                        {/*<input type="radio" classID="sortLastName"*/}
+                        {/*name="sortType" value="Last Name"*/}
+                        {/*checked={this.props.currentSortType === "Last Name"}*/}
+                        {/*onChange={this.handleSortChange}/>*/}
+                        {/*Last Name*/}
+                        {/*</label>*/}
+                        {/*</div>*/}
+                        {/*<div>*/}
+                        {/*<label>*/}
+                        {/*<input type="radio" classID="sortRecent"*/}
+                        {/*name="sortType" value="Recently Contacted"*/}
+                        {/*checked={this.props.currentSortType === "Recently Contacted"}*/}
+                        {/*onChange={this.handleSortChange}/>*/}
+                        {/*Recently Contacted*/}
+                        {/*</label>*/}
+                        {/*</div>*/}
+                    </div>
+                    {/*old code for filterBy*/}
+                    <div className="filterBy">
+                        {/*<h2>Filter By :</h2>*/}
+                        <div className="expansionBar">
+                            <div className="bar-left">
+                                <h2>Country</h2>
+                                <p>{this.props.filterCountries.size !== 0 ? this.props.filterCountries : "All Countries"}</p>
+                            </div>
                             <i className="material-icons">expand_more</i>
                         </div>
-
-                    </div>
-                    <div className="sortBy">
-                        <div>
-                            <label>
-                                <input type="radio" classID="sortFirstName"
-                                       name="sortType" value="First Name"
-                                       checked={this.props.currentSortType === "First Name"}
-                                       onChange={this.handleSortChange}/>
-                                First Name
-                            </label>
+                        <div className="filterType-countries">
+                            {checkboxCountries}
                         </div>
-                        <div>
-                            <label>
-                                <input type="radio" classID="sortLastName"
-                                       name="sortType" value="Last Name"
-                                       checked={this.props.currentSortType === "Last Name"}
-                                       onChange={this.handleSortChange}/>
-                                Last Name
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                <input type="radio" classID="sortRecent"
-                                       name="sortType" value="Recently Contacted"
-                                       checked={this.props.currentSortType === "Recently Contacted"}
-                                       onChange={this.handleSortChange}/>
-                                Recently Contacted
-                            </label>
-                        </div>
-                    </div>
-
-                    <div className="filterBy">
-                        <p>Filter By :</p>
-                        <p>Country</p>
-                        {checkboxCountries}
                     </div>
                     <div>
                         <label><input className="clearBtn" type="reset" name="ic-back" value="Clear"/>
